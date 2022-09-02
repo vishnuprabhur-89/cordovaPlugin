77147d1a1eba360f52963f62237cbdf9e7f7b878
Vishnu prabhu R <vprabhu@enphaseenergy.com>
2022-09-02 15:25:21 +0530

    cordova react app

---
+++ evsePlugin/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Cordova plugin app
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Testing...
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
