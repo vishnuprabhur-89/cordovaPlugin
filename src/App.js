43fec1634a9b8e81c7abc39869bd6bd3d86e2b7e
Vishnu prabhu R <vprabhu@enphaseenergy.com>
2022-09-02 15:36:15 +0530

    cordova react app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cordova plugin app
+          Cordova plugin app..
         </p>
         <a
           className="App-link"
